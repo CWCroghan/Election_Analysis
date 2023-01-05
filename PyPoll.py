965b685cc41378c3d192ee197812dcf0e785a833
CWCroghan <croghan.carry@gmail.com>
2023-01-05 13:33:47 -0500

    Adding election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -4,28 +4,61 @@
 # 3. The percent of votes for each candidate
 # 4. The max percent of votes and related candidate
 
-# Import datetime class from datetime module
-import datetime as dt
-# Use the now() attribute on the datetime class to get present time.
-now = dt.datetime.now()
-# Print the present time.#
-#print("The current time is ", now)
-
-# Import csv modula
+# Import dependents
 import csv
-
 import os
-# Assign variable for file path
+# Assign variables for file path
 file_to_load = os.path.join('Resources','election_results.csv')
+file_to_save = os.path.join("analysis","election_analysis.txt")
+
+#Intitializing variables
+total_votes = 0
+candidate_list = []
+candidate_votes = {}
+
+winner = ""
+winning_count = 0
+winning_percent = 0.0
 
 # Open election results and read the file
 
 with open(file_to_load) as election_data:
     # Do stuff
     file_reader = csv.reader(election_data)
+    #print header
     header = next(file_reader)
     print(header)
-    #for row in file_reader:
-    #    print(row)
 
-#file_to_save = os.path.join("analysis","election_analysis.txt")
+    #Print data from CSV file
+    for row in file_reader:
+
+        total_votes += 1
+        
+        candidate_name = row[2]
+        
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+
+        #incrementing up votes for current candidate
+        
+        candidate_votes[candidate_name] +=1
+
+print(f"Total votes {total_votes:,}")
+print(candidate_votes)
+
+
+for name in candidate_votes:
+    votes = candidate_votes[name]
+    vote_percent = float(votes)/float(total_votes) * 100
+    print(f'{name}: received {vote_percent:.1f}% of the vote.')
+
+    if winning_count < votes:
+        winning_count = votes
+        winning_percent = vote_percent
+        winner = name
+
+print(f"And the winner is {winner} with total {winning_count} for {winning_percent:.1f}% of the votes")
+
+
+
---
+++ Python_practice0.py
@@ -0,0 +1,10 @@
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if "Arapahoe" in counties and "El Paso" not in counties:
+#   print("Only Arapahoe is in the list of counties.")
+#else:
+#    print("Arapahoe is in the list of counties and El Paso is not")
+
+#dictionary    
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters.")
